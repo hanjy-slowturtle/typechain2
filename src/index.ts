a176119b04faa28923e4cdd9c3960984fbce0195
hanjy-slowturtle <h920915@gmail.com>
2022-03-07 22:59:43 +0900

    ~ #0.6 Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
+interface Person {
+  name: string;
+  age: number;
+  gender: string;
+}
+
 const name = "Hanjy",
   age = 31,
   gender = "male";
+
+const person: Person = {
+  name: "Hanjy",
+  age: 31,
+  gender: "male",
+};
 
 const sayHi = (name: string, age: number, gender: string): void => {
   console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 };
 
+const sayHi2 = ({ name, age, gender }: Person) => {
+  console.log(`${name} ${age} ${gender}`);
+};
+
 sayHi(name, age, gender);
+sayHi2(person);
 
 export {};
